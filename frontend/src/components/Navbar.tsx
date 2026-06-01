c2eb37cdad7ae8458f61420a55673a6b24a0e9e0
Tucas <javilestja3@gmail.com>
2026-05-31 23:30:05 -0400

    Actualizacion de los botones de salir de la sesion iniciada

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -43,7 +43,7 @@ export default function Navbar() {
                 }
               }}
             >
-              Salir
+              Cerrar Sesión
             </button>
           </>
         ) : (
